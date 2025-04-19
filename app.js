e19583e33b47667af742225c23f2ad5dabef934b
KasperNeth <okeowoa244@gmail.com>
2025-04-19 20:37:53 +0100

    fix(birthdaywish): rendering bug

--- app.js
+++ app.js
@@ -3,22 +3,26 @@ const cors = require("cors");
 const helmet = require("helmet")
 const compression = require("compression")
 const router = require("./src/routes/index.route")
+const path = require("path")
 
 
 const app = express();
 
-app.use(helmet())
+app.use(helmet({
+  contentSecurityPolicy: false 
+}));
 app.use(cors("*"));
 app.use(compression());
 app.use(express.json());
-app.use(express.static('view'));
+app.use(express.static(path.join(__dirname, 'view')));
 //all registered routes
 app.use(router);
 
 
-//catch all route for invalid routes
-// app.all('*', (req, res) => {
-//   res.status(404).json({ message: 'Route not found', code: 404 });
-// });
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'view', 'index.html'));
+});
+
 
 module.exports = app;
