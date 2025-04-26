f556016520c197c18223eab41dbd93e6cfd4f8aa
KasperNeth <okeowoa244@gmail.com>
2025-04-26 23:31:19 +0100

    fix(cron-job): use external execution only

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const {connectToMongoDB} = require("./src/utils/db.connection");
 require('dotenv').config();
 const {initScheduledTasks, startKeepAliveApp} = require("./cron.scheduler")
 connectToMongoDB();
-initScheduledTasks(); // Initialize scheduled tasks
-
+//comment out internal scheduler to use external scheduler
+// initScheduledTasks(); // Initialize scheduled tasks
+//use external scheduler to run the job for better performance because of render's free plan which makes the cron-jon to run late and not run at all sometimes and when using both the job will run twice
 
 const PORT = process.env.PORT 
 
